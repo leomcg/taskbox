b38ee862339d5e4b8b8eea9b45e5f6a3345e2e1c
Leonardo Goncalves <leonardo.medeiros@valemobi.com.br>
2022-02-15 09:53:03 -0300

    creating first story

---
+++ src/app/components/task/task.component.ts
@@ -0,0 +1,32 @@
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { Task } from 'src/app/models/task.model';
+
+@Component({
+  selector: 'app-task',
+  templateUrl: './task.component.html',
+  styleUrls: ['./task.component.css'],
+})
+export class TaskComponent {
+  @Input() task: Task;
+
+  @Output()
+  onPinTask = new EventEmitter<Event>();
+
+  @Output()
+  onArchiveTask = new EventEmitter<Event>();
+
+  /**
+   * Component method to trigger the onPin event
+   * @param id string
+   */
+  onPin(id: any) {
+    this.onPinTask.emit(id);
+  }
+  /**
+   * Component method to trigger the onArchive event
+   * @param id string
+   */
+  onArchive(id: any) {
+    this.onArchiveTask.emit(id);
+  }
+}
